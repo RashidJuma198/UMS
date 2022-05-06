1733902d57cdc13957c8f3c20f57e13e9426d262
Rashidjuma198 <rashidjumaoti@gmail.com>
2022-05-06 20:54:16 +0300

    worker

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>symatech.com</title>
+    
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ URL::to('assets/css/bootstrap.css') }}">
+    
+    <link rel="stylesheet" href="{{ URL::to('assets/css/app.css') }}">
+    <link rel="stylesheet" href="{{ URL::to('assets/css/pages/auth.css') }}">
+    {{-- message toastr --}}
+    <link rel="stylesheet" href="http://cdn.bootcss.com/toastr.js/latest/css/toastr.min.css"> 
+    <script src="http://cdn.bootcss.com/jquery/2.2.4/jquery.min.js"></script>
+    <script src="http://cdn.bootcss.com/toastr.js/latest/js/toastr.min.js"></script>
+    
+</head>
+
+
+<body>
+   @yield('content')
+</body>
+</html>
